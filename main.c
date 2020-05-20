3d5714998ee5af423572c45fc9770c0edd0719c2
Ghazi Akel <ghaziaakel@gmail.com>
2020-05-20 14:42:23 +0300

    Revert "Ghazi"

--- main.c
+++ main.c
@@ -4,13 +4,6 @@
 #include "data.h"
 
 int main(int argc, char *argv[]) {
-    char *name;
-    
-    name = malloc(sizeof(char)*45);
-    printf("What is your name?\n");
-    fgets(name, 45, stdin);
-    printf("Your name is: %s", name);
-    free(name);
     
     return 0;
 }
